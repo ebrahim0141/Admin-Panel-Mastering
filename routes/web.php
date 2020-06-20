6cc60ba947ee9fbf153cabd6240dabcd86e62fc1
ebrahim0141 <eme.pust@gmail.com>
2020-06-20 12:47:22 +0600

    add this project for admin panel mastering

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+Route::get('/', function () {
+    
+    return redirect()->route('login');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware'=> 'verified'], function(){
+
+Route::get('/inbox', function(){
+	echo "inbox";
+})->name('inbox');
+Route::get('/calender', function(){
+	echo "calender";
+})->name('calender');
+Route::get('/typography', function(){
+	echo "typography";
+})->name('typography');
+
+});
